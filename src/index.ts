bf08d9b3915d34f3e198aa94fecd6af2d6339f47
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-05-04 12:44:23 +0700

    cofig heroku

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
-import helmet from "helmet";
 import userRoute from "./route/user.router"
 import commonRoute from "./route/common.router"
 dotenv.config();
@@ -10,8 +8,6 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-app.use(helmet());
-app.use(cors());
 app.use(express.json());
 userRoute(app)
 commonRoute(app)
